fcf0ad08d7451a6d07fa9c0013b8dee0253a23a2
adrianB3 <balanescuadrian71@gmail.com>
2019-04-17 08:57:07 +0300

    update

---
+++ rsa/rsa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace rsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] plaintext = System.Text.Encoding.ASCII.GetBytes("hello");
+            var key_size = 2048;
+            RSACryptoServiceProvider myrsa1 = new RSACryptoServiceProvider(key_size);
+            var paramss = myrsa1.ExportParameters(true);
+            RSACryptoServiceProvider myrsa2 = new RSACryptoServiceProvider();
+            myrsa2.ImportParameters(paramss);
+
+            byte[] cyphertext = myrsa1.Encrypt(plaintext, true);
+            Console.WriteLine("Plain text: " + System.Text.Encoding.Default.GetString(plaintext));
+            Console.WriteLine("Cypher text RSA: " + System.Text.Encoding.Default.GetString(cyphertext));
+
+            byte[] dec_text = myrsa2.Decrypt(cyphertext, true);
+            Console.WriteLine("Dec text: " + System.Text.Encoding.Default.GetString(dec_text));
+            
+
+        }
+    }
+}
